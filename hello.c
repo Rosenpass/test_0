9c1de2a6523b7763491b410dbb033401ce5cbead
wudu <albertineangerer@gmail.com>
2022-12-30 22:12:42 +0800

    Added a new hello world c program

---
+++ hello.c
@@ -0,0 +1,15 @@
+/*
+ * @Author: error: git config user.name && git config user.email & please set dead value or install git
+ * @Date: 2022-12-30 22:09:46
+ * @LastEditors: error: git config user.name && git config user.email & please set dead value or install git
+ * @LastEditTime: 2022-12-30 22:10:56
+ * @FilePath: \12_28_4d:\2023_1\gitkanken\test_0\hello.c
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
+#include <stdio.h>
+
+int main()
+{
+    printf("Hello,World!");
+    return 0;
+}
